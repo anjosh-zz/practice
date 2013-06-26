788b4c557ed7c3f018f2eae04b226c3f331c09df
anjosh <joshuaan1@gmail.com>
2013-06-26 22:26:02 +0000

    update

--- c/learn-c-the-hard-way/object.c
+++ c/learn-c-the-hard-way/object.c
@@ -4,12 +4,21 @@
 #include "object.h"
 #include <assert.h>
 
-void Objct_destroy(void *self)
-{
-	Object *obj = self;
+//void Object_destroy(void *self)
+//{
+//	Object *obj = self;
+//
+//	if(obj) {
+//		if(obj->description) free(obj->description);
+//		free(obj);
+//	}
+//
+//}
 
-	if(obj) {
-		if(obj->description) free(obj->description);
+void Object_destroy(void *self)
+{
+	if(self) {
+		if(self->description) free(self->description);
 		free(obj);
 	}
 }
@@ -42,20 +51,5 @@ void *Object_new(size_t size, Object proto, char *description)
 {
 	// setup the default functions in case they aren't set
 	if(!proto.init) proto.init = Object_init;
-	if(!proto.describe) proto.describe = Object_describe;
-	if(!proto.destroy) proto.destroy = Object_destroy;
-	if(!proto.attack) proto.attack = Object_attack;
-	if(!proto.move) proto.move = Object_move;
-
-	// this seems weird, but we can make a struct of one size,
-	// then point a different pointer at it to "case" it
-	Object *el = calloc(1, size);
-	*el = proto;
-
-	// copy the description over
-	el-> description = strdup(description);
-
-	// initialize it with whatever init we were given
-	
 
 }
--- c/learn-c-the-hard-way/object.h
+++ c/learn-c-the-hard-way/object.h
@@ -1,7 +1,7 @@
 #ifndef _object_h
 #define _object_h
 
-typedef_enum {
+typedef enum {
 	NORTH, SOUTH, EAST, WEST
 } Direction;
 
@@ -14,12 +14,14 @@ typedef struct {
 	int (*attack)(void *self, int damage);
 } Object;
 
-int Object_init(void *self);
+int Object_int(void *self);
 void Object_destroy(void *self);
 void Object_describe(void *self);
-void *Ovject_move(void *self, Direction direction);
+void *Object_move(void *self, Direction direction);
 int Object_attack(void *self, int damage);
 void *Object_new(size_t size, Object proto, char *description);
 
 #define NEW(T, N) Object_new(sizeof(T), T##Proto, N)
 #define _(N) proto.N
+
+#endif
